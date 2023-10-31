8dee3ce9488050a3bae2db36d16ad9f756989391
Paulo Ribeiro <paulo.ribeirodecarvalho@epfl.ch>
2023-10-31 16:02:18 +0100

    Loadind the data in the notebook and create the helpers.py file.

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def insight(x: pd.DataFrame) -> pd.DataFrame:
+    """
+    Look at the dataframe and return a well-structured DataFrame resuming information of x.
+
+    :param x: DataFrame wanted to be analyzed.
+    :return: Resumed information/insight of DataFrame x.
+    """
+    # TODO: Creata a function returning all information asked by TAs. Refer to notebook, first Markdown.
+    pass
+

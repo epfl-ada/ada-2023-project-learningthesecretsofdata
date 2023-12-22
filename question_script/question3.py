ca87a5f42841c84f5e248525e123647abc875886
JorisMonnet <joris.monnet@free.fr>
2023-12-22 14:48:22 +0100

    Finish Q3

--- question_script/question3.py
+++ question_script/question3.py
@@ -1,7 +1,19 @@
+import plotly.express as px
+
 from helpers import *
+from question_script.question_helper import extract_composers_dataframe
 
 
 def prepare_data_for_q3():
+    """
+    Prepare the data for the question 3:
+    - Load the cleaned and enriched data set (created via enrich_movie_data.py script)
+    - Drop all the columns which are not release date and composer
+    - Drop all the rows which have no composer or no release date
+    - Explode the composer column to have only one composer per row
+
+    :return:
+    """
     # Load the cleaned and enriched data set (created via enrich_movie_data.py script)
     enhanced_movies = pd.read_pickle('dataset/clean_enrich_movies.pickle')
 
@@ -11,29 +23,183 @@ def prepare_data_for_q3():
     # Drop all the rows which have no composer or no release date
     movies = movies.dropna()
 
-    display(movies.head())
-    print(movies.shape)
+    movies = extract_composers_dataframe(movies, False)
 
-    # Create a column composer containing only one composer and add new rows for each composer
-    movie_with_composer = movies.explode('composers')
+    # Keep only c_id and c_name
+    movies = movies[['release_date', 'c_id', 'c_name', 'box_office_revenue']]
 
-    # Rename the column composers to composer
-    movie_renamed = movie_with_composer.rename(columns={'composers': 'composer', 'release_date': 'release_year'})
-
-    movie_renamed = movie_renamed.reset_index(drop=True)
+    # Rename the columns
+    movie_renamed = movies.rename(
+        columns={'release_date': 'release_year', 'c_id': 'composer_id', 'c_name': 'composer_name'})
 
     return movie_renamed
 
 
 def group_by_composer_id(df):
-    df['composer'] = df['composer'].apply(lambda x: x.id)
-
-    # group by composer
-    return df.groupby('composer')
+    """
+    Group the dataframe by composer id
+    :param df:
+    :return:
+    """
+    return df.groupby('composer_id')
 
 
 def filter_by_top_composers(df, nb_top_composers=10):
-    top_composers = df['composer'].value_counts().head(nb_top_composers).index
+    """
+    Filter the dataframe by the composers with the most movies
+    :param df:
+    :param nb_top_composers:
+    :return:
+    """
+    top_composers = df['composer_id'].value_counts().head(nb_top_composers).index
 
     # Keep only the top 10 composers with their index
-    return df[df['composer'].isin(top_composers)]
+    return df[df['composer_id'].isin(top_composers)]
+
+
+def create_plotly_number_of_movies(movie_grouped_by_top_composer):
+    """
+    Save the plotly figure for the number of movies per composer
+    :param movie_grouped_by_top_composer: The dataframe grouped by composer
+    :return: None
+    """
+    new_df = movie_grouped_by_top_composer.copy()
+
+    new_df.dropna(inplace=True)
+
+    # Create a column count which contains the number of movies per year and per composer
+    # Group by composer and year bin, count the number of movies
+    movie_counts = movie_grouped_by_top_composer.groupby(['composer_name', 'year_bin']).size()
+
+    # Unstack the 'composer_name' level to create a DataFrame
+    movie_counts_df = movie_counts.unstack(level='composer_name')
+
+    # Create a df with the year bins and composer_name as columns
+    movie_counts_df = movie_counts_df.reset_index()
+
+    # Rename the columns
+    movie_counts_df.columns = ['year_bin'] + list(movie_counts_df.columns[1:])
+    movie_counts_df = movie_counts_df.sort_values(by='year_bin')
+
+    movie_counts_df['year_bin'] = movie_counts_df['year_bin'].astype(str)
+
+    fig = px.line(movie_counts_df, x='year_bin', y=list(movie_counts_df.columns[1:]),
+                  title='Number of movies per composer')
+
+    # Get the list of unique composers
+    composers = new_df['composer_name'].unique()
+
+    # Add a dropdown menu to select the composers to display
+    dropdown = []
+    for i, composer in enumerate(composers):
+        visible = [False] * len(composers)
+        visible[i] = True
+        dropdown.append(dict(
+            method='update',
+            label=composer,
+            args=[{'visible': visible},
+                  {'title': composer}]))
+    all_button = dict(
+        method='update',
+        label='All',
+        args=[{'visible': new_df['composer_name'].isin(new_df['composer_name'].unique())},
+              {'title': 'All'}])
+
+    # Prepend the "All" button to the dropdown list
+    dropdown.insert(0, all_button)
+
+    dropdown.sort(key=lambda x: x['label'])
+
+    # Add Dropdown menu and select All by default
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                buttons=dropdown,
+                direction="down",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=1.0,
+                xanchor="left",
+                y=1.32,
+                yanchor="top"
+            ),
+        ]
+    )
+
+    fig.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Number of Movies",
+    )
+    fig.update_traces(mode='lines')
+
+    fig.write_html("Q3_number_of_movies_per_year.html")
+
+
+def create_plotly_box_office_revenue(movie_grouped_by_top_composer):
+    """
+    Save the plotly figure for the box office revenue per composer
+    :param movie_grouped_by_top_composer: The dataframe grouped by composer
+    :return: None
+    """
+    new_df = movie_grouped_by_top_composer.copy()
+
+    new_df.dropna(inplace=True)
+    new_df['year_bin'] = new_df['year_bin'].astype(str)
+
+    # Sum the box office revenue per year and per composer
+    new_df = new_df.groupby(['composer_name', 'year_bin'])['box_office_revenue'].sum().reset_index()
+
+    new_df = new_df.sort_values(by='year_bin')
+
+    fig = px.line(new_df, x='year_bin', y='box_office_revenue', color='composer_name',
+                  title='Sum of the Box-Office Revenues per composer')
+
+    # Get the list of unique composers
+    composers = new_df['composer_name'].unique()
+
+    # Add a dropdown menu to select the composers to display
+    dropdown = []
+    for i, composer in enumerate(composers):
+        visible = [False] * len(composers)
+        visible[i] = True
+        dropdown.append(dict(
+            method='update',
+            label=composer,
+            args=[{'visible': visible},
+                  {'title': composer}]))
+
+    all_button = dict(
+        method='update',
+        label='All',
+        args=[{'visible': new_df['composer_name'].isin(new_df['composer_name'].unique())},
+              {'title': 'All'}])
+
+    # Prepend the "All" button to the dropdown list
+    dropdown.insert(0, all_button)
+
+    dropdown.sort(key=lambda x: x['label'])
+
+    # Add Dropdown menu and select All by default
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                buttons=dropdown,
+                direction="down",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=1.0,
+                xanchor="left",
+                y=1.32,
+                yanchor="top"
+            ),
+        ]
+    )
+
+    fig.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Sum of the Box-Office Revenues",
+    )
+
+    fig.update_traces(mode='lines')
+
+    fig.write_html("Q3_box_office_revenue_per_year.html")
